aac88d5bbba43e0cc22e5c3c0cd6a915ec9c8f51
rubelsec <rubelmrm1492@gmail.com>
2026-05-11 17:15:26 +0600

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Everything you want to know about us!</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Home/Header/Header.jsx
@@ -0,0 +1,19 @@
+
+import './Header.css';
+import { Link } from 'react-router-dom';
+const Header = () => {
+    return (
+        <div>
+          
+            <nav>
+                <span> My Website</span>
+                <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/about">About</Link>
+               <Link to="/contact">ContactUs</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
