88cb476201a1702331660b5453fa09eaef0826b5
rubelsec <rubelmrm1492@gmail.com>
2026-05-12 04:12:55 +0600

    added some dynamic route

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,37 @@
+// import { useLoaderData } from "react-router-dom";
+
+// const Users=()=>{
+//     const users=useLoaderData();
+// }
+
+// const users = () => {
+//     return (
+//         <div>
+//             <h2>Our Users:{users.length}</h2>
+//             <p> Fantastic and vodro users </p>
+//         </div>
+//     );
+// };
+
+// export default Users;
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Users.css';
+const Users = () => {
+
+    const users = useLoaderData();
+
+    return (
+        <div>
+            <h2>Our Users: {users.length}</h2>
+            <p>Fantastic and vodro users</p>
+            <div className="users">
+                {
+                    users.map(user=><User key={user.id} user={user}></User>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Users from './components/Users/Users';
+import UserDetails from './components/UserDeatils/UserDetails';
 
 const router=createBrowserRouter([
 {path:'/',
@@ -23,7 +25,13 @@ children:[
   },
   {
     path:'/users',
-    element:<Contact></Contact>
+    loader:()=> fetch('https://jsonplaceholder.typicode.com/users'),
+    element:<Users></Users>
+  },
+  {
+    path:'/user/:userId',
+    loader:({params})=>fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+    element:<UserDetails></UserDetails>
   }
 ]
 },
